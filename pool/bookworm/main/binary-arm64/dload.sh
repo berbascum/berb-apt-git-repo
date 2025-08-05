5bfe6258e4dcc58e5a48f04ef63769eec1e7c907
berbascum <berbascum@ticv.cat>
2025-08-05 23:30:22 +0200

    (bookworm) main: Remove script used to download packages
    Signed-off-by: berbascum <berbascum@ticv.cat>

--- pool/bookworm/main/binary-arm64/dload.sh
+++
@@ -1,5 +0,0 @@
-apt-get download clang-13 gcc-12-base icu-devtools libclang-common-13-dev libclang-cpp13 libclang1-13 libedit2 libffi-dev libgc1 libgcc-12-dev libicu-dev libllvm13 libobjc-12-dev libobjc4 libpfm4 libstdc++-12-dev libxml2-dev libz3-4 libz3-dev llvm-13 llvm-13-dev llvm-13-linker-tools llvm-13-runtime llvm-13-tools python3-pygments python3-yaml
-
-
-
-clang-13 libclang-common-13-dev libclang-cpp13 libclang1-13  libllvm13 llvm-13 llvm-13-dev llvm-13-linker-tools llvm-13-runtime llvm-13-tools
